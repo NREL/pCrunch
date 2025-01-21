a28d71d4969a900acee08c96e2f3bcef36e10501
Garrett Barter <garrett.barter@nrel.gov>
2025-01-20 20:12:17 -0700

    fix notebook example

--- pCrunch/__init__.py
+++ pCrunch/__init__.py
@@ -1,3 +1,3 @@
-from .analysis import Crunch, FatigueParams
+from .crunch import Crunch, FatigueParams
 from .openfast_readers import OpenFASTAscii, OpenFASTBinary, load_FAST_out, read
 from .aeroelastic_output import AeroelasticOutput
